f0769b000f1c822b9340de2663707fa66584c6f5
Natachai-127 <injanatachai@gmail.com>
2025-12-07 16:44:38 +0700

    Update html101.blade.php

--- resources/views/html101.blade.php
+++ resources/views/html101.blade.php
@@ -6,24 +6,26 @@
     <title>Workshop-67160127-g2</title>
 </head>
 <body>
-  <div class="row g-3 align-items-center">
+  <div class="align-items-center">
     <div class="col-auto">
       <label for="fname">First name</label>
       <input type="text" id="fname" name="fname">
+      <label for="sname">Last name</label>
+      <input type="text" id="sname" name="sname">
     </div>
   </div>
-  <div class="col-auto">
-      <label for="sname">Surname</label>
-      <input type="text" id="fname" name="fname">
-  </div>
-    <div class="row g-3 align-items-center">
+    <div class="align-items-center">
   <div class="col-auto">
     <label for="inputPassword6" class="col-form-label">Password</label>
     <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
   </div>
-  <div class="row g-3 align-items-center">
-    <label for="date">Day/Month/Year</label>
+  <div class="align-items-center">
+    <label for="date">Birth Date</label>
     <input type="datetime-local">
+  </div>
+  <div class="align-items-center">
+    <label for="age">Age</label>
+    <input type="text" id="age" name="age">
   </div>
 </div>
 </body>
